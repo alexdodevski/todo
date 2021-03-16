fb0c7a0a52052715a6bdb25248d6076ee653c857
Alexander Dodevski <36091237+alexdodevski@users.noreply.github.com>
2021-03-16 21:34:58 +0300

    90% done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let taskInput = document.querySelector('.task_input')
 let plus = document.querySelector('.plus')
 let keys = Object.keys(localStorage)
 let dateElem = document.querySelector('.date')
+let selectInput = document.querySelector('.select_btn')
 let currentDate = new Date()
 
 
@@ -47,6 +48,11 @@ let methods = {
     }
 }
 
+// method replaceAt
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
 // function constructor
 function Todo() {
     this.favorite = false
@@ -55,78 +61,92 @@ function Todo() {
 }
 
 
-for (let key of keys) {
-    let item = JSON.parse(localStorage.getItem(key))
-    console.log(item)
-    if (item.current == true) {
-        console.log(item.div)
-        taskPanel.innerHTML += item.div
+// добавление тасок в туду 
+
+function displayTodos() {
+    for (let key of keys) {
+        let item = JSON.parse(localStorage.getItem(key))
+        if (item.current == true) {
+            taskPanel.innerHTML += item.div
+        }
     }
 }
 
+displayTodos()
 
-// checked.addEventListener('click', function() {
-//     let todo = new Todo();
-//     // todo.div = document.createElement('div')
-//     // todo.div.classList.add('task')
-//     // todo.div.innerHTML = `<div class="circle"><i class="far fa-circle"></i></div><div class="task_text">${taskInput.value}</div><div class="garbage"><i class="far fa-trash-alt"></i></div><div class="star"><i class=" far fa-star"></i></div>`
-//     // todo.div.setAttribute('currentTodo', new Date())
-//     todo.div = `<div class="task" currentTodo="${new Date()}"><div class="circle"><i class="far fa-circle"></i></div><div class="task_text">${taskInput.value}</div><div class="garbage"><i class="far fa-trash-alt"></i></div><div class="star"><i class=" far fa-star"></i></div></div>`
-//     console.log(todo)
-//     console.log(todo.div)
-//     taskPanel.innerHTML += todo.div
-//     localStorage.setItem(new Date(), JSON.stringify(todo))
-//     taskInput.value = ''
-//     console.log(localStorage)
-// })
+
+// create and delete todo function, and edit status todo.
+
 
 function createTodo() {
-    if (taskInput.value === "" || taskInput.value === " ") return
+    let data = taskInput.value
+    if (data === "" || data == null || data.trim() === "") return false
     let todo = new Todo();
     todo.div = `<div class="task" currentTodo="${new Date()}"><div class="circle"><i class="far fa-circle"></i></div><div class="task_text">${taskInput.value}</div><div class="garbage"><i class="far fa-trash-alt"></i></div><div class="star"><i class=" far fa-star"></i></div></div>`
-    console.log(todo)
-    console.log(todo.div)
     taskPanel.innerHTML += todo.div
     localStorage.setItem(new Date(), JSON.stringify(todo))
     taskInput.value = ''
-    console.log(localStorage)
 }
 
-
-
 function removeTodo(elem) {
-    console.dir(elem)
     let curChangeElem = elem.parentElement.getAttribute('currentTodo')
     elem.parentElement.remove()
     localStorage.removeItem(curChangeElem)
 }
 
 function starEdit(elem) {
+
     let starSolid = '<i class="fas fa-star"></i>'
-    if (elem.innerHTML == starSolid) {
-        elem.innerHTML = '<i class="far fa-star"></i>'
-    } else {
-        elem.innerHTML = starSolid
-    }
     let curChangeElem = elem.parentElement.getAttribute('currentTodo')
     let obj = JSON.parse(localStorage.getItem(curChangeElem))
+
+    if (elem.innerHTML == starSolid) {
+        elem.innerHTML = '<i class="far fa-star"></i>'
+        obj.div = elem.parentElement.outerHTML
+        if (selectInput.value != 'finished') {
+            Array.from(taskPanel.children).forEach(item => {
+                if (item.getAttribute('currentTodo') === curChangeElem) {
+                    item.remove()
+                }
+            })
+        }
+    } else {
+        elem.innerHTML = starSolid
+        obj.div = elem.parentElement.outerHTML
+    }
+
+
     Object.setPrototypeOf(obj, methods)
-    console.log(obj)
     obj.changeFavorite()
     localStorage.setItem(curChangeElem, JSON.stringify(obj))
 }
 
 function doneEdit(elem) {
+
     let elemDone = '<i class="fas fa-check-circle"></i>'
-    if (elem.innerHTML == elemDone) {
-        elem.innerHTML = '<i class="far fa-circle"></i>'
-    } else {
-        elem.innerHTML = elemDone
-    }
     let curChangeElem = elem.parentElement.getAttribute('currentTodo')
     let obj = JSON.parse(localStorage.getItem(curChangeElem))
+
+    if (elem.innerHTML == elemDone) {
+        elem.innerHTML = '<i class="far fa-circle"></i>'
+        obj.div = elem.parentElement.outerHTML
+        Array.from(taskPanel.children).forEach(item => {
+            if (item.getAttribute('currentTodo') === curChangeElem) {
+                item.remove()
+            }
+        })
+    } else {
+
+        elem.innerHTML = elemDone
+        obj.div = elem.parentElement.outerHTML
+        Array.from(taskPanel.children).forEach(item => {
+            if (item.getAttribute('currentTodo') === curChangeElem) {
+                item.remove()
+            }
+        })
+    }
+
     Object.setPrototypeOf(obj, methods)
-    console.log(obj)
     obj.changeFinished()
     obj.changeCurrent()
     localStorage.setItem(curChangeElem, JSON.stringify(obj))
@@ -159,7 +179,7 @@ taskInput.addEventListener('blur', function() {
     plusRemove()
 })
 
-
+// event on task panel ,when click on star, done or garbage icons
 
 taskPanel.addEventListener('click', function(e) {
     if (e.target.parentElement.classList.contains('star')) {
@@ -168,5 +188,18 @@ taskPanel.addEventListener('click', function(e) {
         doneEdit(e.target.parentElement)
     } else if (e.target.parentElement.classList.contains('garbage')) {
         removeTodo(e.target.parentElement)
+    }
+})
+
+// select onchange
+
+selectInput.addEventListener('change', function() {
+    let keys = Object.keys(localStorage)
+    taskPanel.innerHTML = ''
+    for (let key of keys) {
+        let item = JSON.parse(localStorage.getItem(key))
+        if (item[selectInput.value] == true) {
+            taskPanel.innerHTML += item.div
+        }
     }
 })
